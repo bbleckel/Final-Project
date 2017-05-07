2b6311f1f75635c0572df9cc5b898e3a7b9b05cd
Dylan H. Parsons <dylan.parsons1@gmail.com>
2017-05-07 15:30:31 -0400

    Merging

--- FileReader.java
+++ FileReader.java
@@ -5,6 +5,7 @@
 import java.util.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.AlphaComposite;
 
 import java.io.*;
 
@@ -25,7 +26,9 @@ public class FileReader {
             height = image.getHeight();
             pixels = new Color[width][height];
             
+            BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
             BufferedImage grayScale = image;
+//            bi.getGraphics().drawImage(image, 0, 0, width, height, null);
             
             for(int i = 0; i < height; i++){
                 for(int j = 0; j < width; j++){
@@ -35,18 +38,41 @@ public class FileReader {
                     int b = c.getBlue();
                     int a = c.getAlpha();
                     
+//                    System.out.println("Alpha is " + a);
+
                     // convert to grayscale
                     int gr = (r + g + b) / 3;
                     Color gColor = new Color(gr, gr, gr, a);
 
                     grayScale.setRGB(j, i, gColor.getRGB());
+                    bi.setRGB(j, i, gColor.getRGB());
+//                    System.out.println("Set to " + gColor.getAlpha());
                     
                     pixels[j][i] = gColor;
                 }
             }
             
+//            for(int i = 50; i < 100; i++) {
+//                for(int j = 50; j < 100; j++) {
+//                    Color d = new Color(1f, 0f, 0f, .1f);
+//                    int rgb = d.getRGB();
+//                    int alpha = (rgb >> 24) & 0xff;
+//                    System.out.println(d.getRGB() + ", " + alpha);
+//                    System.out.println("d has alpha " + d.getAlpha());
+////                    grayScale.setRGB(j, i, d.getRGB());
+//                    bi.setRGB(j, i, d.getRGB());
+//                    Color c = new Color(bi.getRGB(j, i), true);
+//                    int r = c.getRed();
+//                    int g = c.getGreen();
+//                    int b = c.getBlue();
+//                    int a = c.getAlpha();
+//                    System.out.println("New has r " + r + ", g " + g + ", b " + b + ", a " + a);
+//                }
+//            }
+            
             // write to file
-            ImageIO.write(grayScale, "jpg", new File("./gray.jpg"));
+            ImageIO.write(bi, "jpg", new File("./gray.jpg"));
+            ImageIO.write(grayScale, "jpg", new File("./gray2.jpg"));
             
         } catch (Exception e) {
             System.out.println("Error reading file!");
